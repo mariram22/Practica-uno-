83ea5a50164f14c2912d53b24c8f46e378254e0c
Mariana Ramírez U <mariana.ramirez.uribe61@gmail.com>
2024-08-29 06:30:08 -0500

    created script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+1. Copiar url del repositorio que se va a copiar 
+2. Usar git clone URL  para descargar el repositorio
+3. crear un archivo 
+4. add . para agregar el cambio 
+5. git commit para agregar mensajes de lo que se realizo para que se agregue a la rama 
+6. Git pull para actualizar la carpeta 
+7. luego en git hub se pone push para mandar los cambios al repositorio 
+
